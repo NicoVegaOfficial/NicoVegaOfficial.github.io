1a285bf59d26f155cae0a7de2c5cdd6438d309d9
Nicolás <nicolasvegaguerrero10@gmail.com>
2023-05-18 16:39:34 -0300

    se actualiza la pagina de inicio

--- script.js
+++
@@ -1,8 +0,0 @@
-function abrirMenu() {
-    var idMenu = document.getElementById("myTopnav");
-    if (idMenu.className === "topnav") {
-        idMenu.className += " responsive";
-    } else {
-        idMenu.className = "topnav";
-    }
-}
